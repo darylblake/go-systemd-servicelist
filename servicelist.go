c505d759f33254b45ab95199232cac19e0b6ac57
Daryl Blake <darylblake@gmail.com>
2022-02-18 10:25:16 +1300

    updates'

--- servicelist.go
+++ servicelist.go
@@ -1,51 +1,72 @@
 package go_systemd_servicelist
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 	"strings"
 )
 
 type ServiceItems struct {
-
+	Name 				string			`serviceName`
+	Loaded   			string			``
+	State 				string
+	Status  			string
+	Description 		string
 }
 
-func CollectServiceInfo() error {
-
+func CollectServiceInfo() ([]ServiceItems, error) {
+	serviceItemsList := make([]ServiceItems, 0)
 	cmd := exec.Command("systemctl", "list-units", "--type=service")
 	outputData, err := cmd.Output()
 	if err != nil {
-		return err
+		return serviceItemsList, err
 	}
 
 	log.Println(string(outputData))
 
-	serviceItemsList := make([]ServiceItems, 0)
-	err = processOutputBytesteam( outputData, &serviceItemsList )
+	err = processOutputBytesteam(outputData, &serviceItemsList)
 	if err != nil {
-		return err
+		return serviceItemsList, err
 	}
 
-	return nil
+	return serviceItemsList, nil
 }
 
 
-func processOutputBytesteam(bytestream []byte, d *[]ServiceItems) error {
+func processOutputBytesteam(bytestream []byte, serviceItemsList *[]ServiceItems) error {
+
 	bsString := string(bytestream)
 
 	lines := strings.Split(bsString, "\n")
-	log.Println(bsString)
-
 
 	for k, v := range lines {
 		if k == 0 { //header ?? skip
 			log.Println("Skipping Header Row :P")
-			log.Println(v)
+			continue
 		}
 
-		//todo rest of data...
+		//Process Service List
+		items := strings.Split(v," ")
+		itemLength := len(items)
 
-		//regex..?
+		if itemLength > 4 {
+			si := ServiceItems {
+				Name: items[0],
+				Loaded: items[1],
+				State: items[2],
+				Status: items[3],
+			}
+			desc := ""
+			for i:=4; i < itemLength; i++ {
+
+				desc = fmt.Sprintf("%s %s",desc,items[i])
+			}
+			si.Description = strings.Trim(desc," \n")
+			*serviceItemsList = append(*serviceItemsList, si)
+		} else { //We have ran off the end of the service list. this should be an empty line.
+			return nil
+		}
 	}
 
 	return nil
